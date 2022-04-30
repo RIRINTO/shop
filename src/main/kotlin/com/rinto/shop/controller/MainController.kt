0565dd76204686d54d2aa1ba5067c1e37cac701a
rinto.ri <ririnto@kakao.com>
2022-04-30 17:34:59 +0900

    feature: item Save, show List

--- src/main/kotlin/com/rinto/shop/controller/MainController.kt
+++ src/main/kotlin/com/rinto/shop/controller/MainController.kt
@@ -1,21 +1,42 @@
 package com.rinto.shop.controller
 
+import com.rinto.shop.dto.ItemSaveRequestDTO
 import com.rinto.shop.service.ItemService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 
 @Controller
 class MainController(
     private val itemService: ItemService
 ) {
-    val log:Logger = LoggerFactory.getLogger(this::class.java)
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
 
     @GetMapping
-    fun main(): String {
+    fun main(model: Model): String {
         log.info("main 함수 진입")
 
+        model.addAttribute("items", itemService.itemLists)
+
         return "main"
+    }
+
+    @GetMapping("create")
+    fun create(): String {
+        log.info("show create view")
+
+        return "item/create"
+    }
+
+    @PostMapping("create")
+    fun createSubmit(itemSaveRequestDTO: ItemSaveRequestDTO): String {
+        log.info("submit data: {}", itemSaveRequestDTO)
+
+        itemService.save(itemSaveRequestDTO)
+
+        return "redirect:/"
     }
 }
